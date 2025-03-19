6df508c934209adf2ee445640221ccc81d3d5862
wxgmjfhy <3281412030@qq.com>
2025-03-19 17:38:05 +0800

    feat: 设置监听器容器的并发消费者

--- src/main/java/com/hmdp/listener/VoucherOrderListener.java
+++ src/main/java/com/hmdp/listener/VoucherOrderListener.java
@@ -20,30 +20,19 @@ public class VoucherOrderListener {
     @Resource
     private IVoucherOrderService voucherOrderService;
 
-    public static final String QUEUE_PREFIX = "hmdp.seckill.voucherOrder.queue";
+    public static final String QUEUE_NAME = "hmdp.seckill.voucherOrder.queue";
     public static final String EXCHANGE_NAME = "hmdp.seckill.topic";
     public static final String ROUTINGKEY_NAME = "seckill.order";
 
     @RabbitListener(bindings = @QueueBinding(
-        value = @Queue(name = QUEUE_PREFIX + "1"),
+        value = @Queue(name = QUEUE_NAME),
         exchange = @Exchange(name = EXCHANGE_NAME, type = ExchangeTypes.TOPIC),
         key = ROUTINGKEY_NAME
     ))
-    public void listenVoucherOrderQueue1(VoucherOrder voucherOrder) {
-        log.info("队列 1 接收到秒杀优惠券订单信息: {}", voucherOrder);
+    public void listenVoucherOrder(VoucherOrder voucherOrder) {
+        log.info("接收到秒杀优惠券订单消息: {}", voucherOrder);
         voucherOrderService.createVoucherOrder(voucherOrder);
     }
-
-    @RabbitListener(bindings = @QueueBinding(
-        value = @Queue(name = QUEUE_PREFIX + "2"),
-        exchange = @Exchange(name = EXCHANGE_NAME, type = ExchangeTypes.TOPIC),
-        key = ROUTINGKEY_NAME
-    ))
-    public void listenVoucherOrderQueue2(VoucherOrder voucherOrder) {
-        log.info("队列 2 接收到秒杀优惠券订单信息: {}", voucherOrder);
-        voucherOrderService.createVoucherOrder(voucherOrder);
-    }
-
     
 }
 
